b170ae057c025f151ea154196b977c6e73866ac5
Ken McCormack <9336679+kenleoni@users.noreply.github.com>
2019-03-23 17:17:10 +1000

    Add create ingredient feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -72,3 +72,10 @@ class PrivateTagsApiTests(TestCase):
             name=payload['name']
         ).exists()
         self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid data"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
